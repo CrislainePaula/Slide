897490bbb63593c337746263430d60501885800f
Crislaine Paula <crislaineppp@gmail.com>
2022-04-13 01:29:03 -0300

    first commit

---
+++ interacao/script.js
@@ -0,0 +1,16 @@
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+setInterval(next, 7000);
